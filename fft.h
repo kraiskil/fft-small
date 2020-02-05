bf2a98892ed41879bd86c9e5e30147da2cc15694
Kalle Raiskila <kraiskil@iki.fi>
2020-02-06 00:25:10 +0200

    Make header compile as C++.
    'double complex' does not parse as C++, but std::complex<double>
    is binary compatible.

--- fft.h
+++ fft.h
@@ -3,8 +3,14 @@
  */
 
 #include <stdint.h>
-#include <complex.h>
+#if __cplusplus
+#   include <complex>
+    typedef std::complex< double > cdouble;
+#else
+#   include <complex.h>
+    typedef double complex cdouble;
+#endif
 
-void fft_slow(const int* in, double complex* out, uint16_t N);
+void fft_slow(const int* in, cdouble* out, uint16_t N);
 
-void fft(const int* in, double complex* out, uint16_t N);
+void fft(const int* in, cdouble* out, uint16_t N);
